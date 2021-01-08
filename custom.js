eeeb0ea93db2e869a473e21be6b1657daa73f980
AaryaKumar1 <73297516+AaryaKumar1@users.noreply.github.com>
2021-01-08 11:26:18 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,142 @@
+
+	// Menu Dropdown Toggle
+	if($('.menu-trigger').length){
+		$(".menu-trigger").on('click', function() {
+			$(this).toggleClass('active');
+			$('.header-area .nav').slideToggle(200);
+		});
+	}
+
+
+	// Menu elevator animation
+	$('a[href*=\\#]:not([href=\\#])').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				var width = $(window).width();
+				if(width < 991) {
+					$('.menu-trigger').removeClass('active');
+					$('.header-area .nav').slideUp(200);
+				}
+				$('html,body').animate({
+					scrollTop: (target.offset().top) - 130
+				}, 700);
+				return false;
+			}
+		}
+	});
+ /* jQuery Pre loader
+  -----------------------------------------------*/
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+});
+
+
+/* HTML document is loaded. DOM is ready.
+-------------------------------------------*/
+$(document).ready(function() {
+
+  /* template navigation
+  -----------------------------------------------*/
+ $('.main-navigation').onePageNav({
+        scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
+        scrollOffset: 75, //Height of Navigation Bar
+        filter: ':not(.external)',
+        changeHash: true
+    });
+
+    /* Navigation visible on Scroll */
+    mainNav();
+    $(window).scroll(function () {
+        mainNav();
+    });
+
+    function mainNav() {
+        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        if (top > 40) $('.sticky-navigation').stop().animate({
+            "opacity": '1',
+            "top": '0'
+        });
+        else $('.sticky-navigation').stop().animate({
+            "opacity": '0',
+            "top": '-75'
+        });
+    }
+
+
+   /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+  /*  smoothscroll
+  ----------------------------------------------*/
+   $(function() {
+        $('.navbar-default a, #home a, #overview a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+
+ /* Parallax section
+    -----------------------------------------------*/
+  function initParallax() {
+    $('#home').parallax("100%", 0.1);
+    $('#overview').parallax("100%", 0.3);
+    $('#trainer').parallax("100%", 0.2);
+    $('#newsletter').parallax("100%", 0.3);
+    $('#blog').parallax("100%", 0.1);
+    $('#price').parallax("100%", 0.2);
+    $('#testimonial').parallax("100%", 0.2);
+
+  }
+  initParallax();
+
+
+   /* home slider section
+  -----------------------------------------------*/
+
+
+
+  /* Owl Carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-testimonial").owlCarousel({
+      autoPlay: 6000,
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1],
+      itemsTablet: [768,1],
+      itemsTabletSmall: false,
+      itemsMobile : [479,1],
+    });
+  });
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
+	/* Back top
+-----------------------------------------------*/
+	$(window).scroll(function() {
+			if ($(this).scrollTop() > 200) {
+			$('.go-top').fadeIn(200);
+			} else {
+				$('.go-top').fadeOut(200);
+			}
+			});
+			// Animate the scroll to top
+		$('.go-top').click(function(event) {
+			event.preventDefault();
+		$('html, body').animate({scrollTop: 0}, 300);
+		})
